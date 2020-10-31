8d3acff66537c0eba8f5803365a7195dd4c7d59a
BakPeter <bak.pet@gmail.com>
2020-10-31 14:54:06 +0200

    commit 3

--- Quize/ViewController.swift
+++ Quize/ViewController.swift
@@ -27,7 +27,50 @@ class ViewController: UIViewController {
         let question: String = questions[currentQuestionInd]
         questionLabel.text = question
         answerLabel.text = "???"
+        
+        let name = "peter"
+        var hasPet: Bool? = true
+        hasPet = nil
+    
+        
+        var num: Float64 = 0
+        var arr: Array<Int> = []
+        var strs: [String] = []
+        var dic: Dictionary<String, Int> = [:]
 
+        let countingUp = ["one", "'two'"]
+        countingUp.count
+        
+        for str in countingUp {
+            print(str)
+        }
+        for (i, str) in countingUp.enumerated() {
+            print("(\(i), \(str))")
+        }
+        
+        let num1: Int? = nil
+        let num2: Int?
+        let num3: Int?
+        
+        num2 = 4
+        num3 = 8
+        if let n1 = num1,
+            let n2 = num2,
+            let n3 = num3 {
+                let avg = (n1 + n2 + n3)/3
+                   print(avg)
+        }else {
+            print("nil")
+        }
+        let compareAscending = { (i: Int, j: Int) -> Bool in
+            return i<j
+        }
+        print(compareAscending(1,2))
+        print(compareAscending(2,1))
+        
+        var numbers = [42,9,12,-17]
+        numbers.sort(by:compareAscending)
+        print(numbers)
     }
     
     @IBAction func showAnswer(_ sender: UIButton) {
